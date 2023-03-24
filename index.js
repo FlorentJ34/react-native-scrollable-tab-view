10bd558a9dd00ccc6dc45f3a34189d25ec823749
Florent Jacquemin <florent.jacquemin@outlook.com>
2023-03-24 17:06:28 -0500

    Fix tab scrolling for android

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ const {
 } = ReactNative;
 
 const TimerMixin = require('react-timer-mixin');
-const ViewPager = require('react-native-pager-view');
+const PagerView = require('react-native-pager-view').default;
 
 const SceneComponent = require('./SceneComponent');
 const DefaultTabBar = require('./DefaultTabBar');
 const ScrollableTabBar = require('./ScrollableTabBar');
 
 const AnimatedViewPagerAndroid = Platform.OS === 'android' ?
-  Animated.createAnimatedComponent(ViewPager) :
+  Animated.createAnimatedComponent(PagerView) :
   undefined;
 
 const ScrollableTabView = createReactClass({
